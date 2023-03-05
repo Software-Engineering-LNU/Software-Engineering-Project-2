1bdde14c95dd8773191c7008c707f80a7a6d9cb4
Sergiy Vengrin <sergiyvengrin19@gmail.com>
2023-03-05 18:57:46 +0200

    #2: Created Asp.net MVC solution. Scaffolded db with Entity Framework.

---
+++ EmployeestWeb/EmployeestWeb.DAL/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeestWeb.DAL.Models
+{
+    public partial class Task
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int Estimation { get; set; }
+        public string Status { get; set; } = null!;
+        public int StoryPoints { get; set; }
+        public long TeamId { get; set; }
+        public long UserId { get; set; }
+
+        public virtual Team Team { get; set; } = null!;
+        public virtual User User { get; set; } = null!;
+    }
+}
