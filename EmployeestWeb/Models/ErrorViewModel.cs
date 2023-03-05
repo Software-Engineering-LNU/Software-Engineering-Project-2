13f3332120d9e8dae9081286d610a7102fa1d1a1
DarkPhoeniix <marina.vovchuk777@gmail.com>
2023-03-05 20:22:29 +0200

    #1: Added GitHub Action to auto-format C# code

--- EmployeestWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EmployeestWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
