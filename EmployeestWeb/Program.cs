253cbea60c4557de12797bb03b5fb82682c40bc5
Viktoria <belya.viktoriya@gmail.com>
2023-03-20 17:01:36 +0200

    #27 added Serilog

--- EmployeestWeb/Program.cs
+++ EmployeestWeb/Program.cs
@@ -4,6 +4,8 @@ namespace EmployeestWeb
     using EmployeestWeb.DAL;
     using EmployeestWeb.DAL.Data;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Logging.Configuration;
+    using Serilog;
 
     public class Program
     {
@@ -14,6 +16,16 @@ namespace EmployeestWeb
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Add logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog(Log.Logger);
+
+            builder.Host.UseSerilog();
 
             // Add services to the container.
             builder.Services.AddBLL();
@@ -44,6 +56,8 @@ namespace EmployeestWeb
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseSerilogRequestLogging();
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -52,7 +66,19 @@ namespace EmployeestWeb
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.Run();
+            try
+            {
+                Log.Information("Application starting up");
+                app.Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "The application failed to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
