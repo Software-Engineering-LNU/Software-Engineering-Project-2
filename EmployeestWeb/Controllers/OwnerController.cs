38f512ed24b788fc146f1df7f704903c2c0bfbfb
unknown <polivka2003@gmail.com>
2023-04-04 01:10:13 +0300

    #56: owner-dashboard-created

---
+++ EmployeestWeb/Controllers/OwnerController.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeestWeb.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class OwnerController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            // Add your code to display the Employee dashboard page
+            return this.View();
+        }
+    }
+}
