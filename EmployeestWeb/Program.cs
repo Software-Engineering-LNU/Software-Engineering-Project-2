8e2a4ce768f91909e027c87d76a05adbdeec3797
MarynaVovchuk <vovchuk.marina3@gmail.com>
2023-04-11 02:19:33 +0300

    #41: fixed bugs after resolving merge conflicts

--- EmployeestWeb/Program.cs
+++ EmployeestWeb/Program.cs
@@ -49,7 +49,6 @@ public class Program
         app.UseRouting();
 
         app.UseAuthorization();
-        
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllerRoute(
