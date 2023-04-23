1df36118b511c8d412679d466e3c356a7eee6775
Arsen Polivka <52714918+ArsenPolivka@users.noreply.github.com>
2023-04-23 12:52:51 +0300

    #58: created owner's workers page

---
+++ EmployeestWeb/Controllers/OwnerController.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeestWeb.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class OwnerController : Controller
+    {
+        public IActionResult Workers()
+        {
+            // Add your code to display the Employee dashboard page
+            return this.View();
+        }
+    }
+}
